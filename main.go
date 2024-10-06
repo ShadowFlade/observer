08614f42e39863958f3b458df5e40c990285a271
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-10-06 18:10:13 +0300

    master: restructuring app

--- main.go
+++ main.go
@@ -1,70 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
-	"strconv"
-	"strings"
-
 	myCmd "github.com/ShadowFlade/observer/cmd"
+	"github.com/ShadowFlade/observer/pkg/logic"
 )
 
 func main() {
-	topStatsCommand := "top -b -n 1 | awk 'NR>7 {print $2, $6, $12}'"
-	cmd := exec.Command("bash", "-c", topStatsCommand)
-	topStats, err := cmd.Output()
-
-	type ProgStat struct {
-		Name     string
-		MemUsage float32
-	}
-	type UserStat struct {
-		Prog          []ProgStat
-		TotalMemUsage float32
-		Name          string
-	}
-
-	userStats := make(map[string]UserStat)
-
-	if err != nil {
-		fmt.Println("Error outputting 'top'")
-	}
-
-	for _, val := range strings.Split(string(topStats), "\n") {
-		splitStr := strings.Split(val, " ")
-
-		if len(splitStr) < 3 {
-			continue
-		}
-
-		user, mem, prog := splitStr[0], splitStr[1], splitStr[2]
-
-		memInt, err := strconv.Atoi(mem)
-		if userStat, ok := userStats[user]; ok {
-
-			if err != nil {
-				fmt.Println("Can't convert user memory usage into int", " ", mem)
-			}
-
-			userStat.TotalMemUsage += float32(memInt)
-			userStat.Prog = append(userStat.Prog, ProgStat{
-				Name:     prog,
-				MemUsage: float32(memInt),
-			})
-			userStats[user] = userStat
-		} else {
-			userStats[user] = UserStat{
-				Prog: []ProgStat{
-					{
-						Name:     prog,
-						MemUsage: float32(memInt),
-					},
-				},
-				Name:          user,
-				TotalMemUsage: float32(memInt),
-			}
-		}
-	}
-	fmt.Println(userStats, " user stats")
+	app := logic.App{DebugState: logic.DEBUG_DEBUG}
+	users, userStats := app.Main("shadowflade")
 	myCmd.Execute()
 }
---
+++ pkg/logic/logic.go
@@ -0,0 +1,99 @@
+package logic
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type DEBUG_STATE int64
+
+const (
+	DEBUG_NONE DEBUG_STATE = iota
+	DEBUG_INFO
+	DEBUG_ERROR
+	DEBUG_DEBUG
+)
+
+type App struct {
+	DebugState DEBUG_STATE
+}
+type TopColumns struct {
+	PID, User, PR, NI, Virt, Res, SHR, S, CPU, Mem, Time, Prog int
+}
+
+type ProgStat struct {
+	Name     string
+	MemUsage float32
+}
+
+type UserStat struct {
+	Prog          []ProgStat
+	TotalMemUsage float32
+	Name          string
+}
+
+func (a *App) Main(onlyUser string) ([]string, UserStat) {
+	topColumns := TopColumns{PID: 1, User: 2, PR: 3, NI: 4, Virt: 5, Res: 6, SHR: 7, S: 8, CPU: 9, Mem: 10, Time: 11, Prog: 12}
+	isSkipHeader := true
+	var header string
+
+	if isSkipHeader {
+		header = "NR>7"
+	} else {
+		header = ""
+	}
+	topStatsCommand := fmt.Sprintf("top -b -n 1 | awk '%s {print $%d, $%d, $%d}'", header, topColumns.User, topColumns.Res, topColumns.Prog)
+
+	fmt.Println(topStatsCommand, " top stats command")
+
+	cmd := exec.Command("bash", "-c", topStatsCommand)
+	topStats, err := cmd.Output()
+
+	userStats := make(map[string]UserStat)
+
+	if err != nil {
+		fmt.Println("Error outputting 'top'")
+	}
+
+	for _, val := range strings.Split(string(topStats), "\n") {
+		splitStr := strings.Split(val, " ")
+
+		user, mem, prog := splitStr[0], splitStr[1], splitStr[2]
+
+		//later can refocator that if we pass onluUser we can sort columhns beforehande - so before topStatsCommand so we get only one user - we sort on user, and after stopped getting these users commands we stop parsing
+
+		if len(splitStr) < 3 || (onlyUser != "" && user != onlyUser) {
+			continue
+		}
+
+		memInt, err := strconv.Atoi(mem)
+
+		if userStat, ok := userStats[user]; ok {
+
+			if err != nil {
+				fmt.Println("Can't convert user memory usage into int", " ", mem)
+			}
+
+			userStat.TotalMemUsage += float32(memInt)
+			userStat.Prog = append(userStat.Prog, ProgStat{
+				Name:     prog,
+				MemUsage: float32(memInt),
+			})
+			userStats[user] = userStat
+		} else {
+			userStats[user] = UserStat{
+				Prog: []ProgStat{
+					{
+						Name:     prog,
+						MemUsage: float32(memInt),
+					},
+				},
+				Name:          user,
+				TotalMemUsage: float32(memInt),
+			}
+		}
+	}
+	fmt.Println(userStats, " user stats")
+}
